e8196841cdbc0f63cffff5e695dd6008fda70f42
mkobar <mike@exlorev.com>
2020-03-24 16:55:32 -0400

    Auto-generated commit

---
+++ assets/data/data.ts
@@ -0,0 +1,25 @@
+export const StatsPieChart: any[] = [
+    {party: 'BJP', electionP: 56},
+    {party: 'INC', electionP: 18},
+    {party: 'AA', electionP: 10},
+    {party: 'CPI', electionP: 5},
+    {party: 'CPI-M', electionP: 5},
+    {party: 'BSP', electionP: 7},
+    {party: 'AITS',  electionP: 10}
+];
+
+export interface Employee {
+    company: string;
+    frequency: number;
+}
+
+export const StatsBarChart: Employee[] = [
+    {company: 'Apple', frequency: 100000},
+    {company: 'IBM', frequency: 80000},
+    {company: 'HP', frequency: 20000},
+    {company: 'Facebook', frequency: 70000},
+    {company: 'TCS', frequency: 12000},
+    {company: 'Google', frequency: 110000},
+    {company: 'Wipro', frequency: 5000},
+    {company: 'EMC', frequency: 4000}
+];
